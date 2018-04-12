9be7a080f7ff5b03ea1afa792c82b598b36aba52
David Ritchey <ritchey0713@gmail.com>
2018-04-12 14:12:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,5 +9,27 @@ class Triangle
     @three = three
   end 
   
-  def kind 
+  def kind(triangle)
+    self.
+    
+     def get_married(person)
+    self.partner = person
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+ 
+  class PartnerError < StandardError
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
+end
+ 
 end

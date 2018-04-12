0f622cfd1bcf5b3a7e2ba81517209d34375fac6c
David Ritchey <ritchey0713@gmail.com>
2018-04-12 15:09:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,10 +28,8 @@ class Triangle
    @side_one + @side_two > @side_three && @side_one + @side_three > @side_two && @side_two + @side_three > @side_one && @side_one != 0 && @side_two != 0 && @side_three != 0 
  end 
    
- 
  class TriangleError < StandardError
-   
- end 
+    end 
    
     
 
